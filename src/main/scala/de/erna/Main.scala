8cb3d37d8263a982b5059af99412f5dbe68b6c49
Eros Candelaresi <eros@hpe.com>
2017-09-18 15:41:20 +0200

    Turned app into Spring Boot/Spring Cloud app

--- src/main/scala/de/erna/Main.scala
+++ src/main/scala/de/erna/Main.scala
@@ -1,5 +1,12 @@
 package de.erna
 
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Main
+
 object Main extends App {
   System.out.println("Hello World!!")
+  SpringApplication.run(classOf[Main], args: _*)
 }
